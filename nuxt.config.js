6a21115cc604da74a87d68368f3942dc92036abb
mlmedia <mike@millionlightsmedia.com>
2022-10-18 11:56:34 -0400

    - changing data source url to Yalo demo site

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,80 @@
-import axios from "axios"
+import axios from "axios";
+
+export const wp_json_url = "https://headlessdemoml.wpengine.com/wp-json/wp/v2";
+
 let dynamicRoutes = () => {
-  const routes = axios
-    .get("https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20")
-    .then(res => {
-      return res.data.map(post => `/blog/${post.slug}`)
-    })
-  console.log(routes)
-  return routes
+	const routes = axios
+		.get(wp_json_url + "/posts?page=1&per_page=20")
+		.then(res => {
+			return res.data.map(post => `/blog/${post.slug}`)
+		})
+	console.log(routes)
+	return routes
 }
 
 export default {
-  mode: "universal",
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || "",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Alata|Open+Sans&display=swap"
-      }
-    ]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
-  css: ["~/assets/mixins.scss"],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    "~/plugins/posts.server.js",
-    "~/plugins/tags.server.js",
-    "~/plugins/dateformat.js"
-  ],
-  generate: {
-    routes: dynamicRoutes
-  },
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
-  }
+	mode: "universal",
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: process.env.npm_package_name || "",
+		meta: [{
+				charset: "utf-8"
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1"
+			},
+			{
+				hid: "description",
+				name: "description",
+				content: process.env.npm_package_description || ""
+			}
+		],
+		link: [{
+				rel: "icon",
+				type: "image/x-icon",
+				href: "/favicon.ico"
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css?family=Alata|Open+Sans&display=swap"
+			}
+		]
+	},
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: {
+		color: "#fff"
+	},
+	/*
+	 ** Global CSS
+	 */
+	css: ["~/assets/mixins.scss"],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [
+		"~/plugins/posts.server.js",
+		"~/plugins/tags.server.js",
+		"~/plugins/dateformat.js"
+	],
+	generate: {
+		routes: dynamicRoutes
+	},
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [],
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {}
+	}
 }
