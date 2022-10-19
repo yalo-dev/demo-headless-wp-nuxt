a844027d2e4d6436d58f7ef9a31c41c9325f8f4a
mlmedia <mike@millionlightsmedia.com>
2022-10-19 00:11:37 -0400

    - attempted fix for blog path issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ let dynamicRoutes = () => {
 	const routes = axios
 		.get(wp_json_url + "/posts?page=1&per_page=20")
 		.then(res => {
-			return res.data.map(post => `/blog/${post.slug}`)
+			return res.data.map(post => `/${post.slug}`)
 		})
 	console.log(routes)
 	return routes
